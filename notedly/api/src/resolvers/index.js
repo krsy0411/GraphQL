196f346188478357ee0c055039e0ef21d2d91c8b
PANdas <krsy0411@naver.com>
2024-12-30 21:20:40 +0900

    [Fix] 중첩 쿼리 import 추가 수정

--- notedly/api/src/resolvers/index.js
+++ notedly/api/src/resolvers/index.js
@@ -1,9 +1,13 @@
-const Query = require("./query");
-const Mutation = require("./mutation");
-const { GraphQLDateTime } = require("graphql-iso-date");
+const Query = require('./query');
+const Mutation = require('./mutation');
+const Note = require('./note');
+const User = require('./user');
+const { GraphQLDateTime } = require('graphql-iso-date');
 
 module.exports = {
-    Query,
-    Mutation,
-    DateTime: GraphQLDateTime
-}
+  Query,
+  Mutation,
+  Note,
+  User,
+  DateTime: GraphQLDateTime,
+};
